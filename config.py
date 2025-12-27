7b1c9bf6198204480553ac261b660cb06e06d0e3
tibynx <tibynx@protonmail.com>
2025-12-27 14:07:38 +0100

    Remove unused variables

--- config.py
+++ config.py
@@ -5,5 +5,3 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-BOT_PREFIX = os.getenv('BOT_PREFIX')
-EMBED_COLOR = int(os.getenv('EMBED_COLOR'), 16) if os.getenv('EMBED_COLOR') else 0x7289DA
