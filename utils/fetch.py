7fb82698dd07ae01f7cdc62076f04bf35e8543d0
tibynx <tibynx@protonmail.com>
2025-12-27 19:13:57 +0100

    Removed else statements

--- utils/fetch.py
+++ utils/fetch.py
@@ -4,13 +4,11 @@ async def fetch_json(session, url):
     async with session.get(url) as response:
         if response.status == 200:
             return await response.json()
-        else:
-            return None
+        return None
 
 async def fetch_img(session, url):
     async with session.get(url) as response:
         if response.status == 200:
             image = io.BytesIO(await response.read())
             return image
-        else:
-            return None
+        return None
